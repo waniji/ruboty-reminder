4dd5e1397fb146598e273872fe31ed8aa6e39b69
kaihar4 <kaihar4@gmail.com>
2014-11-17 07:24:14 +0900

    Refactoring

--- lib/ruboty/reminder/task.rb
+++ lib/ruboty/reminder/task.rb
@@ -10,14 +10,21 @@ module Ruboty
       end
 
       def start(robot)
-        current_time = Time.now
-        target = Time.new(current_time.year, current_time.month, current_time.day, hash[:hour], hash[:min], 0)
+        current_time = Time.new.to_i
+        target_time = Time.new(
+          Time.now.year,
+          Time.now.month,
+          Time.now.day,
+          hash[:hour],
+          hash[:min],
+          0
+        ).to_i
 
         wait_time =
-          if target.to_i - current_time.to_i < 0
-            (target + 24*60*60).to_i - current_time.to_i
+          if target_time - current_time < 0
+            (target_time + 24*60*60) - current_time
           else
-            target.to_i - current_time.to_i
+            target_time - current_time
           end
 
         Thread.start do
