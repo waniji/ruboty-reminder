43e3101504ae27a6a5e2b464d54ba54d7d064dcb
kaihar4 <kaihar4@gmail.com>
2014-11-26 18:23:19 +0900

    Improve adding a task

--- lib/ruboty/reminder/task.rb
+++ lib/ruboty/reminder/task.rb
@@ -10,21 +10,23 @@ module Ruboty
       end
 
       def start(robot)
-        current_time = Time.new.to_i
-        target_time = Time.new(
-          Time.now.year,
-          Time.now.month,
-          Time.now.day,
+        time_now = Time.now
+
+        current_unixtime = time_now.to_i
+        target_unixtime = Time.new(
+          time_now.year,
+          time_now.month,
+          time_now.day,
           hash[:hour],
           hash[:min],
           0
         ).to_i
 
         wait_time =
-          if target_time - current_time < 0
-            (target_time + 24*60*60) - current_time
+          if target_unixtime - current_unixtime < 0
+            (target_unixtime + 24*60*60) - current_unixtime
           else
-            target_time - current_time
+            target_unixtime - current_unixtime
           end
 
         Thread.start do
